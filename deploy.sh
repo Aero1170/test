9b3d38c7990f5ce5effd27a27a98ac06533ab242
Yann <yannzeroual@gmail.com>
2023-04-30 00:44:30 +0200

    changement

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+##########################################################################
+#
+# Description: deploiement de docker
+#
+# Auteur: Yann
+#
+#
+##########################################################################
+
+echo "
+
+options:
+   - --create : lancer les contenuers
+   - --drop : supprimer les conteneurs
+   - --info : info sur les conteneurs
+   - --start : relancer les conteneurs
+
+
+"
